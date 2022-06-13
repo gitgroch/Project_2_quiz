4ae2a9937d274c6099bf8c14eddc7898445c9d2b
gitgroch <geoff.rochford@gmail.com>
2022-06-13 12:12:27 +0000

    Add first steps to startQuiz function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 
 // Wait for the DOM to finish loading before starting the survey 
-document.addEventListener("DOMContentLoaded", function(){})
+document.addEventListener("DOMContentLoaded", function(){});
+
+// Global Variables
+
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainer = document.getElementById('question-container');
+const answerContainer = document.getElementById('answer');
+
+// Event Listeners 
+
+startButton.addEventListener('click', startQuiz);
 
 
+/**
+ * Hide Question before start 
+ */
 function startQuiz(){
-
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide');
+    console.log('question shown')
+  
 }
 
 function showQuestion() {
