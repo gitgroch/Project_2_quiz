13d2aaac45a02ad23bd780f0c677aeaf7bbea657
gitgroch <geoff.rochford@gmail.com>
2022-06-13 12:17:42 +0000

    Add first question to question variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,19 +9,31 @@ const nextButton = document.getElementById('next-btn');
 const questionContainer = document.getElementById('question-container');
 const answerContainer = document.getElementById('answer');
 
+const questions = [
+    {
+        question: 'Who played Neo in the 1999 movie The Matrix'
+        answers: [
+            {text: 'Keanue Reeves'},
+            {text: 'Tom Cruise'},
+            {text: 'Will Smith'},
+            {text: 'Nicolas Cage'}
+        ]
+    }
+]
+
 // Event Listeners 
 
 startButton.addEventListener('click', startQuiz);
 
 
 /**
- * Hide Question before start 
+ * Hide Question before start, hide start button when pressed, show question.
  */
 function startQuiz(){
     startButton.classList.add('hide')
     questionContainer.classList.remove('hide');
     console.log('question shown')
-  
+    
 }
 
 function showQuestion() {
