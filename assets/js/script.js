0eeea7bd8a0ace8551b82d4e2be841da651c99e9
gitgroch <geoff.rochford@gmail.com>
2022-06-13 12:33:20 +0000

    Fix syntax in answer array causing an error

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,26 @@
 document.addEventListener("DOMContentLoaded", function(){});
 
 // Global Variables
-
+// Variables from DOM
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainer = document.getElementById('question-container');
 const answerContainer = document.getElementById('answer');
 
+// undefined variable to hold question index 
+let currentQuestionIndex
+
+// list of questions in an array 
 const questions = [
     {
-        question: 'Who played Neo in the 1999 movie The Matrix'
+        question: 'Who played Neo in the 1999 movie The Matrix?',
         answers: [
-            {text: 'Keanue Reeves'},
-            {text: 'Tom Cruise'},
-            {text: 'Will Smith'},
-            {text: 'Nicolas Cage'}
+            { text: 'Keanue Reeves', correct: true },
+            { text: 'Tom Cruise', correct: false },
+            { text: 'Will Smith', correct: false },
+            { text: 'Nicolas Cage', correct: false }
         ]
-    }
+    },
 ]
 
 // Event Listeners 
@@ -30,20 +34,35 @@ startButton.addEventListener('click', startQuiz);
  * Hide Question before start, hide start button when pressed, show question.
  */
 function startQuiz(){
-    startButton.classList.add('hide')
+    // hides start button and shows question 
+    startButton.classList.add('hide');
     questionContainer.classList.remove('hide');
-    console.log('question shown')
+    // sets current question index to 0 (start)
+    currentQuestionIndex = 0;
+    console.log('question shown');
     
 }
 
+/**
+ * Show question from question array 
+ * 
+ */
 function showQuestion() {
 
 }
 
+
+/**
+ * Sets up next question when next button is clicked 
+ */
 function setQuestion() {
 
 }
 
+
+/**
+ * Carries out actions when answer is selected 
+ */
 function selectAnswer  () {
 
 }
