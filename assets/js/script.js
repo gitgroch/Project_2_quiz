6059cda3dc724841444231e51d4daffc0f3a8406
gitgroch <geoff.rochford@gmail.com>
2022-06-13 11:39:31 +0000

    Add required global variables in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,19 @@
 // Wait for the DOM to finish loading before starting the survey 
 document.addEventListener("DOMContentLoaded", function(){})
 
+
 function startQuiz(){
 
 }
 
-function 
+function showQuestion() {
+
+}
+
+function setQuestion() {
+
+}
+
+function selectAnswer  () {
+
+}
