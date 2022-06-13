462c1815618811a2bc6c2469179e5f5d1b2fd506
gitgroch <geoff.rochford@gmail.com>
2022-06-13 12:59:40 +0000

    Add code to randomise question array. Setup showQuestion and setQuestion functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ document.addEventListener("DOMContentLoaded", function(){});
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainer = document.getElementById('question-container');
+const questionElement = document.getElementById('question')
 const answerContainer = document.getElementById('answer');
 
 // undefined variable to hold question index 
-let currentQuestionIndex
+let currentQuestionIndex;
+let randomQuestion;
 
 // list of questions in an array 
 const questions = [
@@ -31,33 +33,40 @@ startButton.addEventListener('click', startQuiz);
 
 
 /**
- * Hide Question before start, hide start button when pressed, show question.
+ * Hide Question before start, hide start button when pressed, then show question.
+ * calls setQuestion function to show 1st question 
  */
 function startQuiz(){
     // hides start button and shows question 
     startButton.classList.add('hide');
     questionContainer.classList.remove('hide');
+    // randomises questions by sorting the elements of the question array and assigning them a random value 
+    randomQuestion = questions.sort(() => Math.random() - .5);
     // sets current question index to 0 (start)
     currentQuestionIndex = 0;
+    setQuestion();
     console.log('question shown');
+
     
 }
 
 /**
- * Show question from question array 
- * 
- */
-function showQuestion() {
-
-}
-
-
-/**
  * Sets up next question when next button is clicked 
  */
-function setQuestion() {
-
+ function setQuestion() {
+    showQuestion(randomQuestion[currentQuestionIndex])
 }
+
+/**
+ * Shows a question object from question array 
+ * 
+ */
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+}
+
+
+
 
 
 /**
