276d419c510ea034afeb1e473f8d41f46147e768
gitgroch <geoff.rochford@gmail.com>
2022-06-14 11:44:06 +0000

    Add more questions, change MAX_QUESTIONS variable to 10

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-
 // Wait for the DOM to finish loading before starting the survey 
-document.addEventListener("DOMContentLoaded", function(){});
+document.addEventListener("DOMContentLoaded", function () {});
 
 // Global Variables
 // Variables from DOM
@@ -23,62 +22,221 @@ let correctAnswers = 0;
 let questionCounter = 0;
 
 // set maximum amount of questions 
-const MAX_QUESTIONS = 5;
+const MAX_QUESTIONS = 10;
 
 // undefined variables to hold question index and the value for random question
 let currentQuestionIndex;
 let randomQuestion;
 
 // list of questions and answers in an array 
-const questions = [
-    {
+const questions = [{
         question: 'What 1999 film did Will Smith pass up to star in Wild Wild West?',
         img: '<img src="assets/media/qimg_www.webp" alt="Will Smith and Kevin Kline in Wild Wild West">',
-        answers: [
-            { text: 'The Matrix', correct: true },
-            { text: 'Dark City', correct: false },
-            { text: 'Star Wars: Episode 1', correct: false },
-            { text: 'The Truman Show', correct: false }
+        answers: [{
+                text: 'The Matrix',
+                correct: true
+            },
+            {
+                text: 'Dark City',
+                correct: false
+            },
+            {
+                text: 'Star Wars: Episode 1',
+                correct: false
+            },
+            {
+                text: 'The Truman Show',
+                correct: false
+            }
         ]
     },
     {
         question: 'What is the name of Ripley\'s cat in "Alien"?',
         img: '<img src="assets/media/qimg_alien_cat.webp" alt="Ripley and her cat from the movie Alien">',
-        answers: [
-            { text: 'Brian', correct: false },
-            { text: 'Spot', correct: false },
-            { text: 'Mitty', correct: false },
-            { text: 'Jonesy', correct: true }
+        answers: [{
+                text: 'Brian',
+                correct: false
+            },
+            {
+                text: 'Spot',
+                correct: false
+            },
+            {
+                text: 'Mitty',
+                correct: false
+            },
+            {
+                text: 'Jonesy',
+                correct: true
+            }
         ]
     },
     {
         question: 'Who played Mad Max in the original Mad Max trilogy?',
         img: '<img src="assets/media/qimg_mad_max.webp" alt="A group of road warrioes from the movie Mad Max">',
-        answers: [
-            { text: 'Keanue Reeves', correct: false },
-            { text: 'Mel Gibson', correct: true },
-            { text: 'Tom Cruise', correct: false },
-            { text: 'Nicolas Cage', correct: false }
+        answers: [{
+                text: 'Keanue Reeves',
+                correct: false
+            },
+            {
+                text: 'Mel Gibson',
+                correct: true
+            },
+            {
+                text: 'Tom Cruise',
+                correct: false
+            },
+            {
+                text: 'Nicolas Cage',
+                correct: false
+            }
         ]
     },
     {
         question: 'In what year does the movie \'Blade Runner\' take place?',
         img: '<img src="assets/media/qimg_bladerunner.webp" alt="Movie art featureing key charachters from the film BladeRunner ">',
-        answers: [
-            { text: '2019', correct: true },
-            { text: '2049', correct: false },
-            { text: '2077', correct: false },
-            { text: '2022', correct: false }
+        answers: [{
+                text: '2019',
+                correct: true
+            },
+            {
+                text: '2049',
+                correct: false
+            },
+            {
+                text: '2077',
+                correct: false
+            },
+            {
+                text: '2022',
+                correct: false
+            }
         ]
     },
     {
         question: 'What is Captain Picard\'s favourite tea?',
         img: '<img src="assets/media/qimg_picard_tea.webp" alt="Captain Picard with a cup of tea">',
-        answers: [
-            { text: 'Matcha ', correct: false },
-            { text: 'Oolong ', correct: false },
-            { text: 'Earl Grey', correct: true },
-            { text: 'Yorkshire Tea', correct: false }
+        answers: [{
+                text: 'Matcha ',
+                correct: false
+            },
+            {
+                text: 'Oolong ',
+                correct: false
+            },
+            {
+                text: 'Earl Grey',
+                correct: true
+            },
+            {
+                text: 'Yorkshire Tea',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What Actor wore the suit of Darth Vader in Star Wars: A New Hope?',
+        img: '<img src="assets/media/qimg_darthvader.webp" alt="Darth Vader choking one of his soldiers">',
+        answers: [{
+                text: 'Hayden Christensen',
+                correct: false
+            },
+            {
+                text: 'James Earl Jones',
+                correct: false
+            },
+            {
+                text: 'David Prowse',
+                correct: true
+            },
+            {
+                text: 'Sebastian Shaw',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What year did Marty McFly travel to in Back to the Future',
+        img: '<img src="assets/media/qimg_back_to_the_future.webp" alt="Marty and Doc experimenting in Back to the future ">',
+        answers: [{
+                text: '1985 ',
+                correct: false
+            },
+            {
+                text: '1885',
+                correct: false
+            },
+            {
+                text: '1995',
+                correct: false
+            },
+            {
+                text: '1955',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Who was not a Captain of the Starship Enterprise',
+        img: '<img src="assets/media/qimg_enterprise_bridge.webp" alt="A view of the bridge of the starship Enterprise">',
+        answers: [{
+                text: 'Kathryn Janeway',
+                correct: true
+            },
+            {
+                text: 'Jean-Luc Picard',
+                correct: false
+            },
+            {
+                text: 'James T. Kirk',
+                correct: false
+            },
+            {
+                text: 'Jonathan Archer',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'What movie was inspired by Philip K. Dicks "Do Androids Dream of Electric Sheep?',
+        img: '<img src="assets/media/qimg_philip_k_dick.webp" alt="An artists impression of author Philip K. Dick">',
+        answers: [{
+                text: 'The Terminator',
+                correct: false
+            },
+            {
+                text: 'A.I.',
+                correct: false
+            },
+            {
+                text: 'Blade Runner',
+                correct: true
+            },
+            {
+                text: 'I, Robot',
+                correct: false
+            }
+        ]
+    },
+    {
+        question: 'In Terminator 2: Judgement Day, what date does Skynet become self-aware signalling the doom of the human race',
+        img: '<img src="assets/media/qimg_terminator.webp" alt="The Terminator adjusts his sunglasses">',
+        answers: [{
+                text: 'February 2, 1983',
+                correct: false
+            },
+            {
+                text: 'August 29, 1997',
+                correct: true
+            },
+            {
+                text: 'January 1, 2001',
+                correct: false
+            },
+            {
+                text: 'May 5, 2029',
+                correct: false
+            }
         ]
     },
 ]
@@ -96,7 +254,7 @@ nextButton.addEventListener('click', () => {
  * Hide Question before start, hide start button when pressed, then show question.
  * calls setQuestion function to show 1st question 
  */
-function startQuiz(){
+function startQuiz() {
     // hides start button and shows question 
     // endButton.classList.add('hide');
     // restartButton.classList.add('hide')
@@ -111,13 +269,13 @@ function startQuiz(){
     setQuestion();
     console.log('question shown');
 
-    
+
 }
 
 /**
  * Sets up next question when next button is clicked 
  */
- function setQuestion() {
+function setQuestion() {
     //  Resets everything to original state  when a new question is set
     resetState()
     //  calls showQuestion function to display a random question (andomQuestion) from the question array (currentQuestionIndex)
@@ -134,7 +292,7 @@ function startQuiz(){
 function showQuestion(question) {
     // Sets inner text of the question Div in the DOM to text from the question array
     questionElement.innerText = question.question;
-    questionImage.innerHTML =question.img;
+    questionImage.innerHTML = question.img;
     //  A loop for all possible answers 
     question.answers.forEach(answer => {
         const button = document.createElement('button')
@@ -153,10 +311,10 @@ function showQuestion(question) {
 /**
  * Carries out actions when answer is selected 
  */
-function selectAnswer  (e) {
-    const selectedButton = e.target 
+function selectAnswer(e) {
+    const selectedButton = e.target
     const correct = selectedButton.dataset.correct
-    if (correct){
+    if (correct) {
         correctAnswers++
         currentScore.innerText = correctAnswers
         console.log(correctAnswers)
@@ -172,8 +330,8 @@ function selectAnswer  (e) {
         return window.location.assign('end.html')
         correctAnswers = 0
     }
-    }
-    
+}
+
 
 
 
@@ -192,13 +350,9 @@ function clearStatusClass(element) {
     element.classList.remove('wrong')
 }
 
-function resetState (){
+function resetState() {
     nextButton.classList.add('hide')
     while (answerContainer.firstChild) {
-        answerContainer.removeChild
-        (answerContainer.firstChild)
+        answerContainer.removeChild(answerContainer.firstChild)
     }
 }
-
-
-
