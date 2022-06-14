f8b7daed4aee355968454e43ffdf2d2bf8a424f1
gitgroch <geoff.rochford@gmail.com>
2022-06-14 13:28:24 +0000

    Clean up highscore.js and end.js, add further notation

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,20 +4,23 @@ const username = document.getElementById('username')
 // identifies save score button
 const saveScoreBtn = document.getElementById('save-score-btn')
 const finalScore = document.getElementById('final-score')
-// Pulls most recent score from 
+// Sets variable to the value of the mostRecentScore stored locally 
 const mostRecentScore = localStorage.getItem('mostRecentScore')
-
+// sets variable to parsed JSON string or an empty array 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-const MAX_HIGH_SCORES = 5 
+// Sets maximum highscoes to include 
+const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
-console.log(mostRecentScore)
 
+// event listener for form when an input is detected 
 username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value 
+    saveScoreBtn.disabled = !username.value
 })
 
+/**
+ * Sets a variable tp hold score and name object, then sorts them to display the highest score first.
+ */
 saveHighScore = e => {
     e.preventDefault()
 
@@ -28,12 +31,13 @@ saveHighScore = e => {
 
     highScores.push(score)
 
-    highScores.sort((a,b) => {
+    highScores.sort((a, b) => {
         return b.score - a.score
     })
 
+    // Removes and adds scores based on value
     highScores.splice(5)
-
+    // convert highscores object to JSON string 
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('index.html')
 }
